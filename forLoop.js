1eb008ae081c5256d80ceddd2923db605ce9ccb3
Your Name <you@example.com>
2024-07-01 12:44:39 +0600

    first commit

---
+++ forLoop.js
@@ -0,0 +1,33 @@
+/* var roast = 0;
+
+while (roast <= 20) {
+    console.log(roast);
+    roast++
+} */
+
+//  for frist time look
+
+/* for (var roast = 0; roast <= 20; roast++) {
+    console.log(roast);
+} */
+
+
+    // for actually look this 
+
+/* for (var r = 0; r <= 12; r++) {
+    console.log(r);
+} */
+
+
+// even number 
+
+/* for (var r = 0; r <= 20; r+=2) {
+    console.log(r);
+}
+ */
+
+// odd number 
+
+for (var r = 1; r <= 20; r+=2) {
+    console.log(r);
+}
---
+++ while.js
@@ -0,0 +1,30 @@
+/****************************
+ var sweet = 0;
+
+while (sweet <= 10) {
+    console.log(sweet);
+    console.log("sweet given");
+    sweet++   
+} 
+********************************/
+
+// even number
+
+/*
+ var num = 0;
+
+while (num <= 15) {
+    console.log(num);
+    var num = num + 2;
+} 
+    */
+
+
+// odd number
+
+var num = 1;
+
+while (num <= 15) {
+    console.log(num);
+    var num = num + 2;
+} 
